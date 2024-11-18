fe611ec7efe735b4693c417eef50d21e271e9a3b
Sixto-J <jvera@cifpfbmoll.eu>
2024-11-18 18:49:38 +0100

    añadido efecto de pulsado y ayudándose del color picker

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.calculadora;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button b1 = (Button) findViewById(R.id.igual);
+
+        b1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
+                    v.setBackgroundColor(Color.parseColor("#2596be"));}
+                if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_BUTTON_RELEASE) {
+                    v.setBackgroundColor(Color.parseColor("#6854a4"));}
+                return false;
+            }
+        });
+
+        Button clear = (Button) findViewById(R.id.clear);
+
+        clear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            //On click function
+            public void onClick(View view) {
+                findViewById(R.id.pantalla);
+
+            }
+        });
+        //Button11.setBackgroundColor(0xFFFF0000);
+
     }
 }
